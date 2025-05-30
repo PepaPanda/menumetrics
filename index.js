427630dcf6c582c42872691d7c2656b87d3403af
Martin Tichý <theidkcze@gmail.com>
2025-05-30 20:05:17 +0200

    Adding cors to be able to continue developing frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 //Modules
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 //Init
@@ -20,6 +21,9 @@ mongoose
   })
 
 app.use(express.json())
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: 'http://localhost:5173' })); //Allow requests from vite dev server in development mode
+}
 
 //Menu items
 const menuItemsRouter = require('./routes/menuitems')
